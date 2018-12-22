96158a653c487ec2897992b9282657c29b9567b0
Fateh Singh <fatehsingh.sunny5@gmail.com>
2018-12-22 21:59:29 +0530

    handled auth

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
+// Passport Config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("its work")
+  res.send('It Works!');
 });
+
+// Use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
-  console.log(`Server started on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
 });
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://root:maro123#@ds127982.mlab.com:27982/storybooks-dev',
+  googleClientID: '266637013864-tq6otq9on9ueoncsnqumst9m0e2ghf90.apps.googleusercontent.com',
+  googleClientSecret: '8iJiTxS5UGCo5CADGBu-j9wz'
+}
