b5748d8fe2fb42d9a8da718bfa62318b8c00f06f
Fateh Singh <fatehsingh.sunny5@gmail.com>
2018-12-23 23:03:49 +0530

    curd for stories

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+//useNewUrlParser
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
-// Load User Model
+// Load Models
 require('./models/User');
+require('./models/Story');
 
 // Passport Config
 require('./config/passport')(passport);
@@ -14,23 +19,45 @@ require('./config/passport')(passport);
 // Load Routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
 // Load Keys
 const keys = require('./config/keys');
+
+// Handlebars Helpers
+const {
+  truncate,
+  stripTags,
+  formatDate,
+  select
+} = require('./helpers/hbs');
 
 // Map global promises
 mongoose.Promise = global.Promise;
 // Mongoose Connect
 mongoose.connect(keys.mongoURI, {
-  useNewUrlParser: true
+  useNewUrlParser:true
 })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
 const app = express();
 
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// MEthod Override Middelware
+app.use(methodOverride('_method'));
+
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
+  helpers: {
+    truncate: truncate,
+    stripTags: stripTags,
+    formatDate:formatDate,
+    select:select
+  },
   defaultLayout:'main'
 }));
 app.set('view engine', 'handlebars');
@@ -52,10 +79,13 @@ app.use((req, res, next) => {
   next();
 });
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Use Routes
 app.use('/', index);
 app.use('/auth', auth);
-
+app.use('/stories', stories);
 
 const port = process.env.PORT || 5000;
 
